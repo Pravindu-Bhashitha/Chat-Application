32e272ab3bb04a6102b61932ef1bf9280af418c5
Pravindu-Bhashitha <pravindubh@gmail.com>
2026-07-23 16:56:54 +0530

    authentication done

--- packages/message-service/src/router/message.routes.ts
+++ packages/message-service/src/router/message.routes.ts
@@ -5,7 +5,7 @@ import authenticateToken from '../middleware/auth.middleware';
 const router = Router();
 
 // Internal/HTTP POST route to save message
-router.post('/', saveMessageController);
+router.post('/', authenticateToken, saveMessageController);
 
 // Authenticated GET route for chat history
 router.get('/:otherUserId', authenticateToken, getConversationController);
